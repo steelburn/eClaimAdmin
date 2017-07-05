39f1d9df4a68a2324c94f6503d573411724e16a4
Unknown <alanlee94@live.com>
2017-07-05 18:05:27 +0800

    Added bank form-try-ing

--- src/pages/banksetup/banksetup.ts
+++ src/pages/banksetup/banksetup.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { FormControlDirective, FormBuilder, Validators, FormGroup,FormControl } from '@angular/forms';
 /**
  * Generated class for the BanksetupPage page.
  *
@@ -13,22 +13,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'banksetup.html',
 })
 export class BanksetupPage {
-
+Bankform: FormGroup;
    public BankRegisterClicked: boolean = false; 
-    // public BankRegisterClick() {
+    public BankRegisterClick() {
 
-    //     this.BankRegisterClicked = !this.BankRegisterClicked;
-    // }
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) 
-  {
-   
-  }
-  BankRegisterClick() {
-     alert('hi');
-
-        //this.BankRegisterClicked = !this.BankRegisterClicked;
+        this.BankRegisterClicked = !this.BankRegisterClicked;
     }
+
+  constructor(private fb: FormBuilder,public navCtrl: NavController, public navParams: NavParams) 
+  {
+   this.Bankform = fb.group({
+      
+      //fullname: ['', Validators.compose([Validators.maxLength(10),Validators.minLength(5), Validators.pattern('[a-zA-Z ]*'), Validators.required])],        
+      
+      bankname:'',
+     
+    });
+  }
+  // BankRegisterClick() {
+  //    alert('hi');
+
+  //       //this.BankRegisterClicked = !this.BankRegisterClicked;
+  //   }
+  
+    
   ionViewDidLoad() {
     console.log('ionViewDidLoad BanksetupPage');
   }
