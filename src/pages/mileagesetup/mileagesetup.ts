00ab8e280178b121293ccc470b8edc997c158086
Pratap Singh <pratap.kshatriya@gmail.com>
2017-08-17 09:32:44 +0530

    Mileagesetup on progress

--- src/pages/mileagesetup/mileagesetup.ts
+++ src/pages/mileagesetup/mileagesetup.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { IonicPage, NavController, NavParams, AlertController  } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
+//import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormControlDirective, FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+import { Http, Headers, RequestOptions, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import * as constants from '../../app/config/constants';
+import { MileageSetup_Model } from '../../models/mileagesetup_model';
+import { MileageSetup_Service } from '../../services/mileagesetup_service';
+import { BaseHttpService } from '../../services/base-http';
+
+import { UUID } from 'angular2-uuid';
 
 /**
  * Generated class for the MileagesetupPage page.
@@ -11,26 +22,95 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @IonicPage()
 @Component({
   selector: 'page-mileagesetup',
-  templateUrl: 'mileagesetup.html',
+  templateUrl: 'mileagesetup.html', providers: [MileageSetup_Service, BaseHttpService]
 })
 export class MileagesetupPage {
- Mileageform: FormGroup;
+  mileage_entry: MileageSetup_Model = new MileageSetup_Model();
+  mileage: MileageSetup_Model = new MileageSetup_Model();
+  Mileageform: FormGroup;
+
+  baseResourceUrl: string = constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/zcs/_table/main_mileage' + '?api_key=' + constants.DREAMFACTORY_API_KEY;
+  baseResource_Url: string = constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/zcs/_table/';
+
+  public mileages: MileageSetup_Model[] = [];
+
+  //public AddMileagesClicked: boolean = false; public EditMileagesClicked: boolean = false; Exist_Record: boolean = false;
+
+  //public AddMileagesClicked: boolean = false; public mileage_details: any;
+
+
+//  Mileageform: FormGroup;
    public AddMileageClicked: boolean = false; 
+   public EditMileageClicked: boolean = false; 
    
     public AddMileageClick() {
 
         this.AddMileageClicked = true; 
     }
+      public EditClick(MILEAGE_GUID: any) {
+    //alert(MILEAGE_GUID);
+    this.EditMileageClicked = true;
+    var self = this;
+    this.mileagesetupservice
+      .get(MILEAGE_GUID)
+      .subscribe((mileage) => self.mileage = mileage);
+    return self.mileage;
+  }
+  public DeleteClick(MILEAGE_GUID: any) {
+    let alert = this.alertCtrl.create({
+      title: 'Remove Confirmation',
+      message: 'Do you want to remove ?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('OK clicked');
+            var self = this;
+            this.mileagesetupservice.remove(MILEAGE_GUID)
+              .subscribe(() => {
+                self.mileages = self.mileages.filter((item) => {
+                  return item.MILEAGE_GUID != MILEAGE_GUID
+                });
+              });
+            //this.navCtrl.setRoot(this.navCtrl.getActive().component);
+          }
+        }
+      ]
+    }); alert.present();
+  }
+
 
       public CloseMileageClick() {
 
-        this.AddMileageClicked = false; 
+        if (this.AddMileageClicked == true) {
+      this.AddMileageClicked = false;
     }
-  constructor(public navCtrl: NavController, public navParams: NavParams,fb:FormBuilder) {
-     this.Mileageform = fb.group({
+    if (this.EditMileageClicked == true) {
+      this.EditMileageClicked = false;
+    }
+    }
+  constructor(public navCtrl: NavController, public navParams: NavParams, fb: FormBuilder, public http: Http, private httpService: BaseHttpService, private mileagesetupservice: MileageSetup_Service, private alertCtrl: AlertController) {
+    this.http
+      .get(this.baseResourceUrl)
+      .map(res => res.json())
+      .subscribe(data => {
+        this.mileages = data.resource;
+      });
+ 
+    this.Mileageform = fb.group({
+      CATEGORY: ["", Validators.required],
+      RATE_PER_UNIT: ["", Validators.required],
+      RATE_DATE: ["", Validators.required],
+      ACTIVATION_FLAG: ["", Validators.required],
+
       
-      mileagename:'',
-     
     });
   }
 
@@ -38,4 +118,48 @@ export class MileagesetupPage {
     console.log('ionViewDidLoad MileagesetupPage');
   }
 
+
+ Save() {
+    if (this.Mileageform.valid) {
+      this.mileage_entry.MILEAGE_GUID = UUID.UUID();
+      this.mileage_entry.CREATION_TS = new Date().toISOString();
+      this.mileage_entry.CREATION_USER_GUID = "1";
+      this.mileage_entry.UPDATE_TS = new Date().toISOString();
+      this.mileage_entry.TENANT_GUID = UUID.UUID();
+      this.mileage_entry.UPDATE_USER_GUID = "";
+      //this.mileage_entry.ACTIVATION_FLAG = boolean;
+      
+      this.mileagesetupservice.save(this.mileage_entry)
+        .subscribe((response) => {
+          if (response.status == 200) {
+            alert('Mileage Registered successfully');
+            //location.reload();
+            this.navCtrl.setRoot(this.navCtrl.getActive().component);
+          }
+        })
+    }
+  }
+Update(MILEAGE_GUID: any) {  
+    if(this.mileage_entry.CATEGORY==null){this.mileage_entry.CATEGORY = this.mileage_entry.CATEGORY;}
+    if(this.mileage_entry.RATE_PER_UNIT==null){this.mileage_entry.RATE_PER_UNIT = this.mileage_entry.RATE_PER_UNIT;}
+
+    if (this.Mileageform.valid) {
+      this.mileage_entry.CREATION_TS = this.mileage.CREATION_TS
+      this.mileage_entry.CREATION_USER_GUID = this.mileage.CREATION_USER_GUID;
+      this.mileage_entry.UPDATE_TS = this.mileage.UPDATE_TS;
+
+      this.mileage_entry.MILEAGE_GUID = MILEAGE_GUID;
+      this.mileage_entry.UPDATE_TS = new Date().toISOString();
+      this.mileage_entry.UPDATE_USER_GUID = '1';
+      
+      this.mileagesetupservice.update(this.mileage_entry)
+        .subscribe((response) => {
+          if (response.status == 200) {
+            alert('Mileage updated successfully');
+            //location.reload();
+            this.navCtrl.setRoot(this.navCtrl.getActive().component);
+          }
+        })
+    }
+  }
 }
---
+++ src/services/mileagesetup_service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, URLSearchParams } from '@angular/http';
+
+import * as constants from '../app/config/constants';
+import { MileageSetup_Model } from '../models/mileagesetup_model';
+import { BaseHttpService } from './base-http';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Observable';
+import { NavController } from 'ionic-angular';
+
+class ServerResponse {
+    constructor(public resource: any) {
+
+    }
+};
+
+@Injectable()
+export class MileageSetup_Service {
+    baseResourceUrl: string = constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/zcs/_table/main_mileage';
+    baseResource_Url: string = constants.DREAMFACTORY_INSTANCE_URL + '/api/v2/zcs/_table/';
+
+    constructor(private httpService: BaseHttpService, private nav: NavController) { };
+
+    private handleError(error: any) {
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.log(errMsg); // log to console instead
+        //localStorage.setItem('session_token', '');       
+        return Observable.throw(errMsg);
+    }    
+
+    save(mileage_main: MileageSetup_Model): Observable<any> {
+        var queryHeaders = new Headers();
+        queryHeaders.append('Content-Type', 'application/json');
+        //queryHeaders.append('X-Dreamfactory-Session-Token', localStorage.getItem('session_token'));
+        queryHeaders.append('X-Dreamfactory-API-Key', constants.DREAMFACTORY_API_KEY);
+        let options = new RequestOptions({ headers: queryHeaders });alert(JSON.stringify(mileage_main));
+        return this.httpService.http.post(this.baseResourceUrl, mileage_main.toJson(true), options)
+            .map((response) => {
+                return response;
+            });
+    }
+
+    update(mileage_main: MileageSetup_Model): Observable<any> {
+        var queryHeaders = new Headers();
+        queryHeaders.append('Content-Type', 'application/json');
+        //queryHeaders.append('X-Dreamfactory-Session-Token', localStorage.getItem('session_token'));
+        queryHeaders.append('X-Dreamfactory-API-Key', constants.DREAMFACTORY_API_KEY);
+        let options = new RequestOptions({ headers: queryHeaders });
+        return this.httpService.http.patch(this.baseResourceUrl, mileage_main.toJson(true), options)
+            .map((response) => {
+                return response;
+            });
+    }
+
+    remove(id: string) {
+        var queryHeaders = new Headers();
+        queryHeaders.append('Content-Type', 'application/json');
+        //queryHeaders.append('X-Dreamfactory-Session-Token', localStorage.getItem('session_token'));
+        queryHeaders.append('X-Dreamfactory-API-Key', constants.DREAMFACTORY_API_KEY);
+        return this.httpService.http
+            .delete(this.baseResourceUrl + '/' + id, { headers: queryHeaders })
+            .map((response) => {
+                var result: any = response.json();
+                return result.DEPARTMENT_GUID;
+            });
+    }
+
+    get(id: string, params?: URLSearchParams): Observable<MileageSetup_Model> {        
+        var queryHeaders = new Headers();
+        queryHeaders.append('Content-Type', 'application/json');
+        //queryHeaders.append('X-Dreamfactory-Session-Token', localStorage.getItem('session_token'));
+        queryHeaders.append('X-Dreamfactory-API-Key', constants.DREAMFACTORY_API_KEY);
+//alert(id);
+        return this.httpService.http
+            .get(this.baseResourceUrl + '/' + id, { search: params, headers: queryHeaders })
+            .map((response) => {
+                var result: any = response.json();
+                let claimtype: MileageSetup_Model = MileageSetup_Model.fromJson(result);
+                return claimtype;
+            }).catch(this.handleError);
+    };
+}
