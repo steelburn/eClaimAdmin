66bd62259111b0494f934b9be5e0e99ce806d2ae
Bijay Kumar Behera <bijay@zen.com.my>
2018-06-28 14:37:15 +0530

    Updated Code

--- src/services/adserver_services.ts
+++ src/services/adserver_services.ts
@@ -16,23 +16,56 @@ class ServerResponse {
 };
 @Injectable()
 export class AdServer_Service {
-    baseResource_Url: string = constants.AD_URL;
-    constructor(private httpService: BaseHttpService) { };
+	baseResource_Url: string = constants.AD_URL;
+	constructor(private httpService: BaseHttpService) { };
 
-    User_Authentication(user_main: UserMain_Model): Observable<any> {
-        debugger;
-        let url: string = this.baseResource_Url + '/user/bijay/authenticate';
-        var queryHeaders = new Headers();
-		
-		// queryHeaders.append("Access-Control-Allow-Origin", "*");
-		// queryHeaders.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept");
+	User_Authentication(user_main: UserMain_Model): Observable<any> {		
+		let url: string = this.baseResource_Url + '/user/bijay/authenticate';
+
+		var queryHeaders = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+		queryHeaders.append('Access-Control-Allow-Origin', "*");
+        queryHeaders.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');        
+        queryHeaders.append('Accept', 'application/json');
 		queryHeaders.append('Content-Type', 'application/json');
 		
-		let options = new RequestOptions({ headers: queryHeaders });
-        //user_main.toJson(true)
-        return this.httpService.http.post(url,{"password": "bijay234"}, options)
+		let options = new RequestOptions({ headers: queryHeaders, withCredentials: true });
+		//user_main.toJson(true)
+		return this.httpService.http.post(url, '{"password": "bijay234"}', options)
+			// .map((response) => {
+			// 	return response;
+			// });
 			.map((response) => {
-				return response;
+				var result: any = response.json();
+				alert(JSON.stringify(result));
+				return result;
 			});
+	}
+
+	GetSingleUser() {
+		debugger;
+		var queryHeaders = new Headers();
+		queryHeaders.append('Access-Control-Allow-Origin', '*');
+		queryHeaders.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+		queryHeaders.append('Accept', 'application/json');
+		queryHeaders.append('Content-Type', 'application/json');
+
+		let options = new RequestOptions({ headers: queryHeaders });
+		let url: string;
+		url = this.baseResource_Url + '/user/bijay';
+
+		return this.httpService.http
+			.get(url, options)
+			.map((response) => {
+				var result: any = response.json();
+				// let bank: BankSetup_Model = BankSetup_Model.fromJson(result); alert("In GetExist" + JSON.stringify(result));
+				return result;
+			}).catch(this.handleError);
+	}
+
+	private handleError(error: any) {
+		let errMsg = (error.message) ? error.message :
+			error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+		console.log(errMsg); // log to console instead    
+		return Observable.throw(errMsg);
 	}
 }
