3b26f99f3e2128414ea68be72ab16787c7823796
Pratap Singh <pratap.kshatriya@gmail.com>
2018-08-24 17:42:51 +0530

    added a remark in view pages

--- src/pages/travel-claim-view/travel-claim-view.component.ts
+++ src/pages/travel-claim-view/travel-claim-view.component.ts
@@ -14,6 +14,7 @@ import { ProfileManagerProvider } from '../../providers/profile-manager.provider
 })
 export class TravelClaimViewPage {
   totalClaimAmount: number = 0;
+  remarks: any;
   claimRequestData: any[];
   claimDetailsData: any[];
   tollParkLookupClicked = false;
@@ -65,6 +66,7 @@ export class TravelClaimViewPage {
           }
           this.TravelType = element.TRAVEL_TYPE === '0' ? 'Local' : 'Outstation';
           this.totalClaimAmount = element.CLAIM_AMOUNT;
+          this.remarks = element.REMARKS;
         });
         //  this.totalClaimAmount += tollorParkAmount;
       })
