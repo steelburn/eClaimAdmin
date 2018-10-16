897e8cc526804468a7c3ee5acf3d7ea88756ccf5
Pratap Singh <pratap.kshatriya@gmail.com>
2018-10-16 20:54:19 +0530

    Issue solved related to profile_json

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -568,7 +568,7 @@ export class LoginPage {
           if (this.KeyNameValueList[item]["KEY_NAME"] == "email_time") { localStorage.setItem("cs_email_time", this.KeyNameValueList[item]["KEY_VALUE"]); }
 
           if (this.KeyNameValueList[item]["KEY_NAME"] == "draft_notification") { localStorage.setItem("draft_notification", this.KeyNameValueList[item]["KEY_VALUE"]); }
-          if (this.KeyNameValueList[item]["KEY_NAME"] == "profile_guid") { localStorage.setItem("profile_guid", this.KeyNameValueList[item]["KEY_VALUE"]); }
+          if (this.KeyNameValueList[item]["KEY_NAME"] == "profile_guid") { localStorage.setItem("cs_profile_guid", this.KeyNameValueList[item]["KEY_VALUE"]); }
         }
       });
   }
