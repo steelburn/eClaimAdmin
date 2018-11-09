cc5771b8ae7ac2c1b7c3071203f9f63f5db032bc
Mohd Khairulnizam <steelburn@KN-MacBook-Pro.local>
2018-11-09 12:18:09 +0800

    Case sensitivity

--- src/pages/permission/permission.module.ts
+++ src/pages/permission/permission.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { PermissionPage } from './Permission';
+import { PermissionPage } from './permission';
 
 @NgModule({
   declarations: [
